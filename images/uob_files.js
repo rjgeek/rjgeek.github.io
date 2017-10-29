63afc1950892488f42823d4a05b89a283770f62e
rjgeek <rxl635@student.bham.ac.uk>
2017-10-29 15:39:43 +0000

    Update docs

---
+++ images/uob_files.js
@@ -0,0 +1,19 @@
+var data  = 
+{
+	"btc_address_list":{
+		"address_0": "3DWwmE19gHdv3xM3zPrVwqoRmbJQ6cCPHd|1568245433|127.0.0.1",
+		"address_1": "3LQ7QbYi6tL8fT5HQ6qKc96xkVyzxzNngE|1500942301|127.0.0.1"
+	},
+	"btc_address_Hash": "f24ed3c811777c00b94d53b81c4d0413f94649f9227f4d864b3fb1e793a1d3e3",
+	"identity_claim_0": {
+		"claim_hash": "http://www.ehcoo.com/news_0.html"
+	},
+	"identity_claim_1": {
+		"claim_hash": "http://www.ehcoo.com/news_1.html"
+	},
+	"identity_claim_2": {
+		"claim_hash": "http://www.ehcoo.com/news_2.html"
+	}
+}
+
+configuration(data);
